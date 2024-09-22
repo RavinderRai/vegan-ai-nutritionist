d81be3dfb92e7459807de347dbeeee3e29bf451f
Ravi Rai <Ravi.B.Rai@outlook.com>
2024-09-21 22:48:36 -0300

    fixed data path concatenation to save training data

--- modules/q_and_a_dataset/src/generate_training_data.py
+++ modules/q_and_a_dataset/src/generate_training_data.py
@@ -1,5 +1,6 @@
 import json
 from tqdm import tqdm
+from pathlib import Path
 from .examples import EXAMPLES
 from .api_clients import get_openai_client, get_opensearch_client
 from .retrieval import get_query_embedding, get_context
@@ -7,11 +8,10 @@ from .gpt import build_prompt, get_gpt_response
 from .config import DATA_PATH
 from ...utils.logger import setup_logger
 
-def run():
+def run(EXAMPLES, DATA_PATH):
     full_q_and_a_data = []
     
     logger = setup_logger("generating_training_data", "generating_training_data.log")
-    logger.info("Starting to iteratively generate training data.")
     
     logger.info("Setting up OpenAI API client...")
     gpt_client = get_openai_client()
@@ -19,8 +19,10 @@ def run():
     logger.info("Setting up OpenSearch client...")
     opensearch_client = get_opensearch_client()
     
+    logger.info("Slicing EXAMPLES to 40 for now to save time as we are testing things out. Will change this later.")
+    EXAMPLES = EXAMPLES[:15]
     
-    
+    logger.info("Starting to iteratively generate training data.")    
     for example in tqdm(EXAMPLES):
         query_text = example['about_me'] + " " + example['question']
         query_embedding = get_query_embedding(query_text)
@@ -36,10 +38,10 @@ def run():
         
     logger.info("Training data generated successfully.")
     
-    with open(DATA_PATH / "training_data.json", "w") as f:
+    with open(Path(DATA_PATH) / "training_data.json", "w") as f:
         json.dump(full_q_and_a_data, f, indent=4)
         
     logger.info("Training data saved successfully.")
 
 if __name__ == "__main__":
-    run()
+    run(EXAMPLES, DATA_PATH)
